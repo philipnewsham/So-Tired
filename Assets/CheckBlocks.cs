137504637cd35c8740eccf48b823a39ed33257d2
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-04-14 18:56:39 +0100

    Uploaded game

---
+++ Assets/CheckBlocks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBlocks : MonoBehaviour
+{
+    int currentBlockCount;
+    public GameObject[] startingPositions;
+    public void AddBlockCount()
+    {
+        currentBlockCount += 1;
+        if(currentBlockCount == 1)
+        {
+            for (int i = 0; i < startingPositions.Length; i++)
+            {
+                startingPositions[i].SetActive(false);
+            }
+        }
+    }
+}
